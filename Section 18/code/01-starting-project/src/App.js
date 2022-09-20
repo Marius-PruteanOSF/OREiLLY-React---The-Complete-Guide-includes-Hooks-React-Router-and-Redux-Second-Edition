149e7b90f7e159b088047aa1abbb5bb65996d00c
Marius-PruteanOSF <marius.prutean@osf.digital>
2022-09-20 10:18:54 +0300

    -part of Redux lerning;

--- Section 18/code/01-starting-project/src/App.js
+++ Section 18/code/01-starting-project/src/App.js
@@ -1,9 +1,20 @@
+import { useSelector } from 'react-redux';
 import Counter from './components/Counter';
-
+import {Fragment} from 'react';
+import Header from './components/Header'
+import Auth from './components/Auth'
+import Profile from './components/UserProfile'
 
 function App() {
+  const isAuth = useSelector(state => state.auth.isAuthenticated);
+
   return (
-    <Counter />
+    <Fragment>
+      <Header />
+      {!isAuth && <Auth />}
+      {isAuth && <Profile />}
+      <Counter />
+    </Fragment>
   );
 }
 
